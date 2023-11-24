6d3be561a0884a13ba96b4cf99837a42a78e7255
seniel98 <jdpadron98@gmail.com>
2023-11-24 09:43:43 +0100

    Removed try catch block from cli.py and main.py

--- cli.py
+++ cli.py
@@ -4,64 +4,61 @@ from main import main
 
 
 def run_cli():
-    try:
-        parser = argparse.ArgumentParser(
-            description="A tool to automate the process of retrieving necessary inputs for the GRAL program.",
-            epilog="""
-    Examples:
-    Retrieve only the map:
+
+    parser = argparse.ArgumentParser(
+        description="A tool to automate the process of retrieving necessary inputs for the GRAL program.",
+        epilog="""
+        Examples:
+        Retrieve only the map:
         python cli.py --base_directory /path/to/base/directory --north 39.49 --south 39.47 --east -0.37 --west -0.39 --epsg 3857 --process map
-            """,
-            formatter_class=argparse.RawTextHelpFormatter
-        )
+        """,
+        formatter_class=argparse.RawTextHelpFormatter
+    )
 
-        parser.add_argument("--base-directory", required=True,
-                            help="The base directory for the project.", dest="base_directory")
-        parser.add_argument("--north", type=float,
-                            help="The northernmost latitude of the bounding box. Must be in EPSG:4326, and decimal degrees.", required=False, dest="north", default=None)
-        parser.add_argument("--south", type=float,
-                            help="The southernmost latitude of the bounding box. Must be in EPSG:4326, and decimal degrees.", required=False, dest="south", default=None)
-        parser.add_argument("--east", type=float,
-                            help="The easternmost longitude of the bounding box. Must be in EPSG:4326, and decimal degrees.", required=False, dest="east", default=None)
-        parser.add_argument("--west", type=float,
-                            help="The westernmost longitude of the bounding box. Must be in EPSG:4326, and decimal degrees.", required=False, dest="west", default=None)
-        parser.add_argument("--epsg", type=int,
-                            help="The EPSG code for the coordinate system to reproject the map to, ie: 3857.", default=3857, dest="epsg")
-        parser.add_argument("--process", choices=["all", "map", "buildings", "weather", "highways", "gral"],
-                            help="Specify the process to run. Choices are: all, map, buildings, weather, highway, gral", required=True,  dest="process")
-        parser.add_argument("--map-name", type=str, default="basemap",
-                            help="The name of the map file to be saved.", dest="map_filename")
-        parser.add_argument("--buildings-shp-file", type=str, default="buildings.shp",
-                            help="The name of the shapefile to be saved.", dest="buildings_shapefile_filename")
-        parser.add_argument("--highways-shp-file", type=str, default="highways.shp", help="The name of the shapefile to be saved.",
-                            dest="highways_shapefile_filename")
-        parser.add_argument("--net-file", type=str, required=True, default="net.net.xml", help="The name of the SUMO network file.",
-                            dest="net_file")
-        parser.add_argument("--emissions-file", type=str, required=True , default="emissions.xml",
-                            help="The name of the SUMO emissions file.", dest="emissions_file")
-        parser.add_argument("--weather-file", type=str,
-                            help="The name of the weather data file.", dest="weather_file")
-        parser.add_argument("--output-weather-file", type=str, default="output_weather_data",
-                            help="The name of the output weather data file.", dest="output_weather_file")
-        parser.add_argument("--weather-day", type=str,
-                            help="The day of the weather data to extract, format (dd.mm.yyyy)", dest="weather_day", default=None)
-        parser.add_argument("--weather-hour", type=str,
-                            help="The hour of the weather data to extract, format (hh:mm)", dest="weather_hour", default=None)
-        parser.add_argument("--met-file", type=str, default="weather.met", help="The name of the met file.",
-                            dest="met_file")
-        parser.add_argument("--gral-dll", type=str,required=True,
-                            help="The name of the GRAL executable.", dest="gral_dll")
-        parser.add_argument("--osm-file", type=str, default=None,
-                            help="The name of the OSM file.", dest="osm_file")
-        parser.add_argument("--online", default=False, action="store_true",
-                            help="The program by default executes the offline mode. If you dont provide any osm or net files it is recommended to use online", dest="is_online")
+    parser.add_argument("--base-directory", required=True,
+                        help="The base directory for the project.", dest="base_directory")
+    parser.add_argument("--north", type=float,
+                        help="The northernmost latitude of the bounding box. Must be in EPSG:4326, and decimal degrees.", required=False, dest="north", default=None)
+    parser.add_argument("--south", type=float,
+                        help="The southernmost latitude of the bounding box. Must be in EPSG:4326, and decimal degrees.", required=False, dest="south", default=None)
+    parser.add_argument("--east", type=float,
+                        help="The easternmost longitude of the bounding box. Must be in EPSG:4326, and decimal degrees.", required=False, dest="east", default=None)
+    parser.add_argument("--west", type=float,
+                        help="The westernmost longitude of the bounding box. Must be in EPSG:4326, and decimal degrees.", required=False, dest="west", default=None)
+    parser.add_argument("--epsg", type=int,
+                        help="The EPSG code for the coordinate system to reproject the map to, ie: 3857.", default=3857, dest="epsg")
+    parser.add_argument("--process", choices=["all", "map", "buildings", "weather", "highways", "gral"],
+                        help="Specify the process to run. Choices are: all, map, buildings, weather, highway, gral", required=True,  dest="process")
+    parser.add_argument("--map-name", type=str, default="basemap",
+                        help="The name of the map file to be saved.", dest="map_filename")
+    parser.add_argument("--buildings-shp-file", type=str, default="buildings.shp",
+                        help="The name of the shapefile to be saved.", dest="buildings_shapefile_filename")
+    parser.add_argument("--highways-shp-file", type=str, default="highways.shp", help="The name of the shapefile to be saved.",
+                        dest="highways_shapefile_filename")
+    parser.add_argument("--net-file", type=str, required=True, default="net.net.xml", help="The name of the SUMO network file.",
+                        dest="net_file")
+    parser.add_argument("--emissions-file", type=str, required=True , default="emissions.xml",
+                        help="The name of the SUMO emissions file.", dest="emissions_file")
+    parser.add_argument("--weather-file", type=str,
+                        help="The name of the weather data file.", dest="weather_file")
+    parser.add_argument("--output-weather-file", type=str, default="output_weather_data",
+                        help="The name of the output weather data file.", dest="output_weather_file")
+    parser.add_argument("--weather-day", type=str,
+                        help="The day of the weather data to extract, format (dd.mm.yyyy)", dest="weather_day", default=None)
+    parser.add_argument("--weather-hour", type=str,
+                        help="The hour of the weather data to extract, format (hh:mm)", dest="weather_hour", default=None)
+    parser.add_argument("--met-file", type=str, default="weather.met", help="The name of the met file.",
+                        dest="met_file")
+    parser.add_argument("--gral-dll", type=str,required=True,
+                        help="The name of the GRAL executable.", dest="gral_dll")
+    parser.add_argument("--osm-file", type=str, default=None,
+                        help="The name of the OSM file.", dest="osm_file")
+    parser.add_argument("--online", default=False, action="store_true",
+                        help="The program by default executes the offline mode. If you dont provide any osm or net files it is recommended to use online", dest="is_online")
 
-        args = parser.parse_args()
+    args = parser.parse_args()
 
-        main(args)
-
-    except Exception as e:
-        raise Exception(f"An error occurred: {e}")
+    main(args)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -41,145 +41,142 @@ def create_shapefile(geo_df, coordinate_system, directory, filename):
 
 
 def main(args):
-    try:
-        # Define location dictionary from args
-        location = {
-            "north": args.north,
-            "south": args.south,
-            "east": args.east,
-            "west": args.west
-        }
 
-        # Create objects for each module
-        buildings_module = Buildings(location)
-        weather_module = Weather(args.base_directory)
-        highways_module = Highways(location)
-        maps_module = Maps(args.base_directory)
-        gral_module = GRAL(args.base_directory, args.met_file,
-                           args.buildings_shapefile_filename, args.highways_shapefile_filename)
-        
-        # Read the SUMO network file
-        net_file = sumo.net.readNet(f'{args.net_file}')
+    # Define location dictionary from args
+    location = {
+        "north": args.north,
+        "south": args.south,
+        "east": args.east,
+        "west": args.west
+    }
 
-        # Process based on the specified argument
-        if args.process in ['all','buildings']:
-            buildings_gdf = buildings_module.process_buildings(args.is_online, osm_file=args.osm_file)
-            create_shapefile(
-                buildings_gdf,
-                f"EPSG:{args.epsg}",
-                args.base_directory,
-                args.buildings_shapefile_filename
-            )
+    # Create objects for each module
+    buildings_module = Buildings(location)
+    weather_module = Weather(args.base_directory)
+    highways_module = Highways(location)
+    maps_module = Maps(args.base_directory)
+    gral_module = GRAL(args.base_directory, args.met_file,
+                        args.buildings_shapefile_filename, args.highways_shapefile_filename)
+    
+    # Read the SUMO network file
+    net_file = sumo.net.readNet(f'{args.net_file}')
 
-        if args.process in ['all', 'weather']:
-            if args.weather_file is None:
-                print("No weather file specified, creating default weather and met files...")
-                weather_df, met_file_df = weather_module.create_default_files()
+    # Process based on the specified argument
+    if args.process in ['all','buildings']:
+        buildings_gdf = buildings_module.process_buildings(args.is_online, osm_file=args.osm_file)
+        create_shapefile(
+            buildings_gdf,
+            f"EPSG:{args.epsg}",
+            args.base_directory,
+            args.buildings_shapefile_filename
+        )
+
+    if args.process in ['all', 'weather']:
+        if args.weather_file is None:
+            print("No weather file specified, creating default weather and met files...")
+            weather_df, met_file_df = weather_module.create_default_files()
+            weather_module.write_to_files(
+                weather_df, f'default_{args.output_weather_file}.csv', False)
+            weather_module.write_to_files(met_file_df, args.met_file)
+        else:
+            weather_df, met_file_df = weather_module.process_weather_data(
+                args.weather_file)
+            weather_module.write_to_files(
+                weather_df, f'{args.output_weather_file}.csv', False)
+            weather_module.write_to_files(met_file_df, args.met_file)
+            if not args.weather_day is None:
+                day_met_file_df = met_file_df[met_file_df['fecha']
+                                            == args.weather_day]
+                # Replace the point with a underscore
+                args.weather_day = args.weather_day.replace('.', '_')
                 weather_module.write_to_files(
-                    weather_df, f'default_{args.output_weather_file}.csv', False)
-                weather_module.write_to_files(met_file_df, args.met_file)
-            else:
-                weather_df, met_file_df = weather_module.process_weather_data(
-                    args.weather_file)
-                weather_module.write_to_files(
-                    weather_df, f'{args.output_weather_file}.csv', False)
-                weather_module.write_to_files(met_file_df, args.met_file)
-                if not args.weather_day is None:
-                    day_met_file_df = met_file_df[met_file_df['fecha']
-                                                == args.weather_day]
-                    # Replace the point with a underscore
-                    args.weather_day = args.weather_day.replace('.', '_')
+                    day_met_file_df, f'{args.met_file}_{args.weather_day}.met')
+                if not args.weather_hour is None:
+                    hour_met_file_df = day_met_file_df[(
+                        day_met_file_df['hora'] == args.weather_hour)]
+                    # Replace the colon with a underscore
+                    args.weather_hour = args.weather_hour.replace(':', '_')
                     weather_module.write_to_files(
-                        day_met_file_df, f'{args.met_file}_{args.weather_day}.met')
-                    if not args.weather_hour is None:
-                        hour_met_file_df = day_met_file_df[(
-                            day_met_file_df['hora'] == args.weather_hour)]
-                        # Replace the colon with a underscore
-                        args.weather_hour = args.weather_hour.replace(':', '_')
-                        weather_module.write_to_files(
-                            hour_met_file_df, f'{args.met_file}_{args.weather_day}_{args.weather_hour}.met')
+                        hour_met_file_df, f'{args.met_file}_{args.weather_day}_{args.weather_hour}.met')
 
-        if args.process in ['all', 'highways']:
+    if args.process in ['all', 'highways']:
 
-            highway_gdf = highways_module.process_highway_data(args.is_online, net_file, args.osm_file)
-            
-            # Read the SUMO emissions file
-            sumo_emissions_df = highways_module.process_sumo_edges_emissions_file(args.is_online,
-                args.emissions_file, net_file, highway_gdf)
-            
+        highway_gdf = highways_module.process_highway_data(args.is_online, net_file, args.osm_file)
+        
+        # Read the SUMO emissions file
+        sumo_emissions_df = highways_module.process_sumo_edges_emissions_file(args.is_online,
+            args.emissions_file, net_file, highway_gdf)
+        
 
-            # Combine the sumo emissions and highway data
-            highway_emissions_gdf = highways_module.combine_sumo_emissions_and_highway_data(args.is_online,
-                sumo_emissions_df, highway_gdf)
-            
-            # Create the shapefile
-            create_shapefile(highway_emissions_gdf,
-                             f"EPSG:{args.epsg}", args.base_directory, args.highways_shapefile_filename)
+        # Combine the sumo emissions and highway data
+        highway_emissions_gdf = highways_module.combine_sumo_emissions_and_highway_data(args.is_online,
+            sumo_emissions_df, highway_gdf)
+        
+        # Create the shapefile
+        create_shapefile(highway_emissions_gdf,
+                            f"EPSG:{args.epsg}", args.base_directory, args.highways_shapefile_filename)
 
-        if args.process in ['map']:
-            if not args.is_online:
-                net_file_processor = NetFileProcessor(net_file)
-                location = net_file_processor.get_bounds_from_net_file()
-            # Convert the coordinates to EPSG
-            west_point_epsg_new_x, north_point_epsg_new_y = maps_module.convert_coordinates(
-                location["west"], location["north"], 4326, args.epsg)
-            east_point_epsg_new_x, south_point_epsg_new_y = maps_module.convert_coordinates(
-                location["east"], location["south"], 4326, args.epsg)
+    if args.process in ['map']:
+        if not args.is_online:
+            net_file_processor = NetFileProcessor(net_file)
+            location = net_file_processor.get_bounds_from_net_file()
+        # Convert the coordinates to EPSG
+        west_point_epsg_new_x, north_point_epsg_new_y = maps_module.convert_coordinates(
+            location["west"], location["north"], 4326, args.epsg)
+        east_point_epsg_new_x, south_point_epsg_new_y = maps_module.convert_coordinates(
+            location["east"], location["south"], 4326, args.epsg)
 
-            # Dictionary with the location coordinates in EPSG:3857
-            location_epsg_new = {"north": north_point_epsg_new_y, "south": south_point_epsg_new_y,
+        # Dictionary with the location coordinates in EPSG:3857
+        location_epsg_new = {"north": north_point_epsg_new_y, "south": south_point_epsg_new_y,
+                            "east": east_point_epsg_new_x, "west": west_point_epsg_new_x}
+        maps_module.create_georeferenced_map(
+            location_epsg_new,
+            args.epsg,
+            args.map_filename
+        )
+    if args.process in ['all', 'gral']:
+        if args.north is None or args.south is None or args.east is None or args.west is None:
+            net_file_processor = NetFileProcessor(net_file)
+            location = net_file_processor.get_bounds_from_net_file()
+        
+        # Generate the files for GRAL executable
+        # Convert the coordinates to EPSG
+        west_point_epsg_new_x, north_point_epsg_new_y = maps_module.convert_coordinates(
+            location["west"], location["north"], 4326, args.epsg)
+        east_point_epsg_new_x, south_point_epsg_new_y = maps_module.convert_coordinates(
+            location["east"], location["south"], 4326, args.epsg)
+
+        # Dictionary with the location coordinates in EPSG:3857
+        location_epsg_new = {"north": north_point_epsg_new_y, "south": south_point_epsg_new_y,
                                 "east": east_point_epsg_new_x, "west": west_point_epsg_new_x}
-            maps_module.create_georeferenced_map(
-                location_epsg_new,
-                args.epsg,
-                args.map_filename
-            )
-        if args.process in ['all', 'gral']:
-            if args.north is None or args.south is None or args.east is None or args.west is None:
-                net_file_processor = NetFileProcessor(net_file)
-                location = net_file_processor.get_bounds_from_net_file()
-            
-            # Generate the files for GRAL executable
-            # Convert the coordinates to EPSG
-            west_point_epsg_new_x, north_point_epsg_new_y = maps_module.convert_coordinates(
-                location["west"], location["north"], 4326, args.epsg)
-            east_point_epsg_new_x, south_point_epsg_new_y = maps_module.convert_coordinates(
-                location["east"], location["south"], 4326, args.epsg)
-
-            # Dictionary with the location coordinates in EPSG:3857
-            location_epsg_new = {"north": north_point_epsg_new_y, "south": south_point_epsg_new_y,
-                                 "east": east_point_epsg_new_x, "west": west_point_epsg_new_x}
-            
-            # Define the pollutant
-            pollutant = "PM10"
-            # Define horizontal layers to simulate in meters
-            horizontal_layers = [3,6,9]
-            # Create the GREB file
-            gral_module.create_greb_file(bbox=location_epsg_new, horizontal_slices=len(horizontal_layers))
-            # Create the in.dat file
-            mean_latitude = (location["north"] + location["south"]) / 2
-            gral_module.create_in_dat_file(particles_ps=100, dispertion_time=3600, latitude=mean_latitude, horizontal_slices=horizontal_layers)
-            # Create the meteoptg.all file
-            gral_module.create_meteopgt_file()
-            # Create the other required files
-            gral_module.create_other_txt_requiered_files(pollutant=pollutant, n_cores=4)
-            # Create the buildings file
-            gral_module.create_buildings_file()
-            # Create the line emission sources file
-            gral_module.create_line_emissions_file(pollutant=pollutant, is_online=args.is_online)
-            # Create the other optional files
-            gral_module.create_other_optional_files()
-            # Go to the base directory
-            os.chdir(args.base_directory)
-            # Run the GRAL executable
-            # subprocess.run(["dotnet", "run", "--project", args.gral_dll], check=True)
-            subprocess.run(["dotnet", args.gral_dll], check=True)
-            # Rename the results files to make them more descriptive
-            n_meteo_conditions = gral_module.get_number_of_weather_conditions()
-            gral_module.rename_results(pollutant=pollutant, horizontal_layers=horizontal_layers, n_meteo_conditions=n_meteo_conditions)
-           
-    except Exception as e:
-        raise Exception(f"An error occurred: {e}")
+        
+        # Define the pollutant
+        pollutant = "PM10"
+        # Define horizontal layers to simulate in meters
+        horizontal_layers = [3,6,9]
+        # Create the GREB file
+        gral_module.create_greb_file(bbox=location_epsg_new, horizontal_slices=len(horizontal_layers))
+        # Create the in.dat file
+        mean_latitude = (location["north"] + location["south"]) / 2
+        gral_module.create_in_dat_file(particles_ps=100, dispertion_time=3600, latitude=mean_latitude, horizontal_slices=horizontal_layers)
+        # Create the meteoptg.all file
+        gral_module.create_meteopgt_file()
+        # Create the other required files
+        gral_module.create_other_txt_requiered_files(pollutant=pollutant, n_cores=4)
+        # Create the buildings file
+        gral_module.create_buildings_file()
+        # Create the line emission sources file
+        gral_module.create_line_emissions_file(pollutant=pollutant, is_online=args.is_online)
+        # Create the other optional files
+        gral_module.create_other_optional_files()
+        # Go to the base directory
+        os.chdir(args.base_directory)
+        # Run the GRAL executable
+        # subprocess.run(["dotnet", "run", "--project", args.gral_dll], check=True)
+        subprocess.run(["dotnet", args.gral_dll], check=True)
+        # Rename the results files to make them more descriptive
+        n_meteo_conditions = gral_module.get_number_of_weather_conditions()
+        gral_module.rename_results(pollutant=pollutant, horizontal_layers=horizontal_layers, n_meteo_conditions=n_meteo_conditions)
 
 
 if __name__ == "__main__":
